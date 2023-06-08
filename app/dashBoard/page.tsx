0c59847b76e6e814fd62220051fbdea1041b8b2e
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-06-08 23:20:39 +0500

    created page

---
+++ app/dashBoard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello dash
+    </div>
+  )
+}
+
+export default page
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main >
+      <h1 className='mx-6'>Hello From Ahmad</h1>
+    </main>
+  )
+}
